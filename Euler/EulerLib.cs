d87d5c0e70a0a3fcc055fef547c04c1993fcb5cb
Hugh <hugh.vanarsdale@live.com>
2023-11-24 07:08:47 -0600

    Done Euler 20 && made hjvaMath library

--- Euler/EulerLib.cs
+++ Euler/EulerLib.cs
@@ -1,18 +1,29 @@
-﻿namespace Euler
+﻿namespace EulerLib
 {
     public static class Problems
     {
+        /*****************************************************************************************
+        * 
+        */
+
+        /******************************************************************************************
+        * In triangle of number find max path from top to bottom
+        * Actually, read data from bottom to top.
+        */
         public static int MaximumPathSum(int[,] matrix){
 
+            // for each row starting on 2nd from last line
             for (int x=matrix.GetLength(0) - 2; x >= 0; x--){
+                // for each col from 0 to length
                 for (int y = 0; y < matrix.GetLength(1); y++){
+                    // add cell to either the one below or the one below and to the right.
                     if (matrix[x,y] != 0){
                         matrix[x,y] = (matrix[x+1, y] > matrix[x+1,y+1]) 
                                     ? matrix[x+1, y] + matrix[x,y]: matrix[x+1,y+1] + matrix[x,y];
                     }
                 }
             }
-
+            // max path will be in 0,0 cell
             return matrix[0,0];
         }
     }
